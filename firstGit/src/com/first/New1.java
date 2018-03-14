5851784cf5201e269e6a292b8a45ab3197f4b23f
zhaoyy0313 <zyuan_87@163.com>
2018-03-14 10:29:05 +0800

    first git

---
+++ firstGit/src/com/first/New1.java
@@ -0,0 +1,9 @@
+package com.first;
+/**
+ * @author zhaoyy E-mail:zhaoyuanyuan@sinovatech.com
+ * @version 创建时间：2018年3月14日 上午10:23:51
+ * 类说明
+ */
+public class New1 {
+
+}
---
+++ firstGit/src/com/second/New2.java
@@ -0,0 +1,9 @@
+package com.second;
+/**
+ * @author zhaoyy E-mail:zhaoyuanyuan@sinovatech.com
+ * @version 创建时间：2018年3月14日 上午10:27:25
+ * 类说明
+ */
+public class New2 {
+
+}
