0f398cbe3fce33e10af9ef90301b02b70d934831
zhaoyy0313 <zyuan_87@163.com>
2018-03-15 14:29:23 +0800

    1

---
+++ firstGit/src/com/first/New1.java
@@ -0,0 +1,14 @@
+package com.first;
+/**
+ * @author zhaoyy E-mail:zhaoyuanyuan@sinovatech.com
+ * @version 创建时间：2018年3月14日 上午10:23:51
+ * 类说明
+ */
+public class New1 {
+	private String cc;
+	private String dd;
+	private String aa;
+	private String bb;
+			
+
+}
